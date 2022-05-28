ac616e65b59535eb3902615c2a758a57981e4660
michaelcmarafino <michaelcmarafino@gmail.com>
2022-05-28 16:37:02 -0400

    added local storage to keep track of games won and games played

--- js/main.js
+++ js/main.js
@@ -1,13 +1,70 @@
-document.querySelector('#pikachu').addEventListener('click', makeReq)
-document.querySelector('#geodude').addEventListener('click', makeReq)
-document.querySelector('#squirtle').addEventListener('click', makeReq)
+document.querySelector("#pikachu").addEventListener("click", makeReq)
+document.querySelector("#geodude").addEventListener("click", makeReq)
+document.querySelector("#squirtle").addEventListener("click", makeReq)
+document
+    .querySelector(".game-counter-btn")
+    .addEventListener("click", clearLocalStorageHistory)
+const gameCounter = document.querySelector(".game-counter")
 
-async function makeReq(e){
-    const userPick = e.target.id;
+// runs at start to render game counter data to the screen
+renderGameCounter()
+
+async function makeReq(e) {
+    // check if there is local storage for the game, if not create it
+    checkForGameCounter()
+
+    const userPick = e.target.id
     const res = await fetch(`/api?pokemon=${userPick}`)
     const data = await res.json()
 
-    console.log(data);
-    document.querySelector("#game-throws").textContent = data.throwMessage;
-    document.querySelector("#win-or-lose").textContent = data.winOrLoseMessage;
+    console.log(data)
+
+    // get gamesWon and gamesPlayed from local storage
+    let gamesWon = JSON.parse(localStorage.getItem("gamesWon"))
+    let gamesPlayed = JSON.parse(localStorage.getItem("gamesPlayed"))
+
+    document.querySelector("#game-throws").textContent = data.throwMessage
+    document.querySelector("#win-or-lose").textContent = data.winOrLoseMessage
+
+    // checks if a game was played by seeing if win-or-lose h2 has content, if it does we add 1 to gamesPlayed variable and then put that value into local storage
+    if (document.querySelector("#win-or-lose").textContent) {
+        gamesPlayed++
+        gamesPlayed = localStorage.setItem(
+            "gamesPlayed",
+            JSON.stringify(gamesPlayed)
+        )
+    }
+
+    // checks if win-or-lose h2 has the "won!" string in it.  If it does then increment the gamesWon variable and put that into local storage
+    if (
+        document.querySelector("#win-or-lose").textContent.search("won!") !== -1
+    ) {
+        gamesWon++
+        gamesWon = localStorage.setItem("gamesWon", JSON.stringify(gamesWon))
+    }
+
+    // renders the new gamesPlayed and gamesWon values to the screen
+    renderGameCounter()
+}
+
+// check if there is a game counter in local storage, if not it creates it and sets it to 0
+function checkForGameCounter() {
+    if (localStorage.getItem("gamesPlayed") == null) {
+        localStorage.setItem("gamesWon", "0")
+        localStorage.setItem("gamesPlayed", "0")
+    }
+}
+
+// renders the new gamesPlayed and gamesWon values to the screen
+function renderGameCounter() {
+    gameCounter.textContent = `Games Won: ${localStorage.getItem(
+        "gamesWon"
+    )}, Games Played: ${localStorage.getItem("gamesPlayed")}`
+}
+
+// clears local storage and then sets it to 0 using checkForGameCounter and then renders it using renderGameCounter
+function clearLocalStorageHistory() {
+    localStorage.clear()
+    checkForGameCounter()
+    renderGameCounter()
 }
--- server.js
+++ server.js
@@ -1,76 +1,80 @@
-const http = require('http');
-const fs = require('fs')
-const url = require('url');
-const querystring = require('querystring');
-const figlet = require('figlet')
+const http = require("http")
+const fs = require("fs")
+const url = require("url")
+const querystring = require("querystring")
+const figlet = require("figlet")
+
+const PORT = 8000
 
 const server = http.createServer((req, res) => {
-	const page = url.parse(req.url).pathname;
-	const params = querystring.parse(url.parse(req.url).query);
-	
-	console.log(page);
-	
-	if (page == '/') {
-		fs.readFile('index.html', function(err, data) {
-		res.writeHead(200, {'Content-Type': 'text/html'});
-		res.write(data);
-		res.end();
-		});
-	}
-	else if (page == '/api') {
-		// store user's throw
-		const userThrow = params['pokemon'];
-		
-		// randomly select computer's throw
-		const pokemon = ['pikachu','geodude','squirtle'];
-		const cpuThrow = pokemon[Math.floor(Math.random() * 3)];
-		
-		// squirtle beats geodude, geodude beats pikachu, picaku beats squirtle
-		let throws = `You choose ${userThrow[0].toUpperCase() + userThrow.slice(1)}. CPU chooses ${cpuThrow[0].toUpperCase() + cpuThrow.slice(1)}.`;
+    const page = url.parse(req.url).pathname
+    const params = querystring.parse(url.parse(req.url).query)
 
-		// game logic
-		const winOrLose = () => {
-			if (userThrow === cpuThrow) {
-				return `It's a draw! Select your next Pokemon.`
-			} else if ((userThrow === `pikachu` && cpuThrow === `squirtle`) || (userThrow === `squirtle` && cpuThrow === `geodude`) || (userThrow === `geodude` && cpuThrow === `pikachu`)) {
-				return `You won! Try your luck again. :)`
-			} else {
-				return `You lost. Try again!`
-			}
-		};
+    console.log(page)
 
-		// send response
-		res.writeHead(200, {'Content-Type': 'application/json'});
-			const objToJson = {
-				throwMessage: throws,
-				winOrLoseMessage: winOrLose()
-			}
-		res.end(JSON.stringify(objToJson));
-	}
-	else if (page == '/css/style.css'){
-		fs.readFile('css/style.css', function(err, data) {
-			res.write(data);
-			res.end();
-		});
-	}
-	else if (page == '/js/main.js'){
-		fs.readFile('js/main.js', function(err, data) {
-			res.writeHead(200, {'Content-Type': 'text/javascript'});
-			res.write(data);
-			res.end();
-		});
-	}
-	else{
-		figlet('404!!', function(err, data) {
-		if (err) {
-			console.log('Something went wrong...');
-			console.dir(err);
-			return;
-		}
-		res.write(data);
-		res.end();
-		});
-	}
-	});
+    if (page == "/") {
+        fs.readFile("index.html", function (err, data) {
+            res.writeHead(200, { "Content-Type": "text/html" })
+            res.write(data)
+            res.end()
+        })
+    } else if (page == "/api") {
+        // store user's throw
+        const userThrow = params["pokemon"]
 
-server.listen(8000);
+        // randomly select computer's throw
+        const pokemon = ["pikachu", "geodude", "squirtle"]
+        const cpuThrow = pokemon[Math.floor(Math.random() * 3)]
+
+        // squirtle beats geodude, geodude beats pikachu, picaku beats squirtle
+        let throws = `You choose ${
+            userThrow[0].toUpperCase() + userThrow.slice(1)
+        }. CPU chooses ${cpuThrow[0].toUpperCase() + cpuThrow.slice(1)}.`
+
+        // game logic
+        const winOrLose = () => {
+            if (userThrow === cpuThrow) {
+                return `It's a draw! Select your next Pokemon.`
+            } else if (
+                (userThrow === `pikachu` && cpuThrow === `squirtle`) ||
+                (userThrow === `squirtle` && cpuThrow === `geodude`) ||
+                (userThrow === `geodude` && cpuThrow === `pikachu`)
+            ) {
+                return `You won! Try your luck again. :)`
+            } else {
+                return `You lost. Try again!`
+            }
+        }
+
+        // send response
+        res.writeHead(200, { "Content-Type": "application/json" })
+        const objToJson = {
+            throwMessage: throws,
+            winOrLoseMessage: winOrLose(),
+        }
+        res.end(JSON.stringify(objToJson))
+    } else if (page == "/css/style.css") {
+        fs.readFile("css/style.css", function (err, data) {
+            res.write(data)
+            res.end()
+        })
+    } else if (page == "/js/main.js") {
+        fs.readFile("js/main.js", function (err, data) {
+            res.writeHead(200, { "Content-Type": "text/javascript" })
+            res.write(data)
+            res.end()
+        })
+    } else {
+        figlet("404!!", function (err, data) {
+            if (err) {
+                console.log("Something went wrong...")
+                console.dir(err)
+                return
+            }
+            res.write(data)
+            res.end()
+        })
+    }
+})
+
+server.listen(PORT, console.log(`Server now running on PORT ${PORT}...`))
